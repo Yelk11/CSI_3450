d4695c5879fa7a785eb9a6e2e769513bce820e46
Yelk11 <neighbour.matthew@gmail.com>
2021-08-05 20:33:17 -0400

    can make multi query from .sql file

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
 
-include 'password.php'
+include "password.php";
 /*
 make a 'password.php' file and copy this text (replacing with correct info)
 <?php
@@ -14,22 +18,22 @@ $db = "username";
 //create connection
 $con=mysqli_connect($server,$userName,$pass,$db);
 
-// Check connection
+//Check connection
 if (mysqli_connect_errno())
 {
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-//example insert statement to add a new mascot
-mysqli_query($con,"INSERT INTO tbl_ouMascots(name, YearCreated)
-VALUES (’Grizz’, 1998)");
-//example SELECT stament to show the results of the mascots table
-$result = mysqli_query($con,"SELECT * FROM ’tbl_ouMascots’ WHERE 1 LIMIT 0, 30; ");
-While($row = mysqli_fetch_array($result))
-{
-echo $row[’name’] . " " . $row[’YearCreated’];
-echo "<br>";
+$sql = file_get_contents("create_table.sql");
+
+if ($con->multi_query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
 }
-mysqli_close($con);
+
+$con->close();
 
 ?>
+</body>
+</html>
