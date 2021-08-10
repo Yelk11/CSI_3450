3cd4c0690506a0b912ef85b9b4b20216724dc30b
mark-srb <88518815+mark-srb@users.noreply.github.com>
2021-08-10 18:20:23 -0400

    Add files via upload

---
+++ Employee login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+include "password.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+make a 'password.php' file and copy this text (replacing with correct info)
+<?php
+$server = "localhost";
+$userName = "username";
+$pass = "password";
+$db = "username";
+?>
+*/
+
+//create connection
+$con = new mysqli($server,$userName,$pass,$db);
+
+//Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+// $result = $con->multi_query() || trigger_error("Query Failed: ".mysqli_error($con), E_USER_ERROR);
+
+// //mysqli query
+// $sql = file_get_contents("create_table.sql");
+// $stmt = $con->multi_query($sql);
+
+
+$con->close();
+
+?>
+
+<form action="Destination form.php" method="post">
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name"><br>
+Employee ID:  <input type="text" name="employee_id"><br>
+<input type="submit"/>
+<button type="button" onclick="history.back();">Back</button>
+</form>
+
+<br>
+<br>
+
+
+
+
+</body>
+
+
+</html>
