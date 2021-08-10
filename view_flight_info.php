c99f3550da6776e01e1c8ae5f2fe0c92caaf6d92
Yelk11 <neighbour.matthew@gmail.com>
2021-08-10 17:44:38 -0400

    update

--- view_flight_info.php
+++ view_flight_info.php
@@ -31,25 +31,48 @@ error_reporting(E_ALL);
 
 //create connection
 $con = mysqli_connect($server,$userName,$pass,$db);
+$flight_id = $_POST["flight_id"];
+$flight_sql = "SELECT * FROM FLIGHT WHERE FLIGHT_ID = $flight_id;";
 
-$sql = "SELECT * FROM FLIGHT;";
 $result = mysqli_query($con, $sql);
 $resultCheck =mysqli_num_rows($result);
 
 if ($resultCheck > 0)
 {
-    while ($row = mysqli_fetch_assoc($result)){
-        echo $row['FLIGHT_ID'];
-        echo "<br>";
-    }
+    $row = mysqli_fetch_assoc($result);
+    echo "Your Flight Id: ". $row['FLIGHT_ID']. "<br>";
+    
+    echo "Departure time is: " . $row['DEPARTURE_TIME']. "<br>";
+
+    echo $row['BOARDING_TIME'];
+
+    echo $row['DURATION'];
+
+    $plane_id = $row['PLANE_ID'];
+    $destination_airport = $row['DESTINATION_AIRPORT'];
+    $source_airport = $row['SOURCE_AIRPORT'];
 }
 
+$plane_sql = "SELECT * FROM PLANE WHERE PLANE_ID = $plane_id";
+
+mysqli_result($result);
+$result = mysqli_query($con, $sql);
+$resultCheck =mysqli_num_rows($result);
+
+if ($resultCheck > 0)
+{
+    $row = mysqli_fetch_assoc($result);
+    
+}
+
+
+
+
+
+echo "Your Plane ;"
 $con->close();
 
 ?>
-
-
-
 
 </body>
 </html>
