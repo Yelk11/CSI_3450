5fd86ad5d00dc52162a7868a8f960349b8c0762c
Yelk11 <neighbour.matthew@gmail.com>
2021-08-11 19:27:50 -0400

    update

--- Destination form.php
+++ Destination form.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html>
 
+CREATE TABLE IF NOT EXISTS FLIGHT (
+    FLIGHT_ID           INTEGER NOT NULL,
+    PLANE_ID            INTEGER NOT NULL,
+    DEPARTURE_TIME      TIME,
+    DESTINATION_AIRPORT INTEGER NOT NULL,
+    BOARDING_TIME       TIME,
+    SOURCE_AIRPORT      INTEGER NOT NULL,
+    DURATION            TIME,
+
+);
 <body>
-    <h1>Pick what city you would like to leave from</h1>
-    <form action="Flights.php" method="post">
+    <h1>Pick your flight</h1>
+    <form action="view_flight_info.php" method="post">
         <select name="source">
             <?php
                 include "password.php";
@@ -11,16 +21,26 @@
                 ini_set('display_startup_errors', 1);
                 error_reporting(E_ALL);
 
-
+                
                 $con = mysqli_connect($server,$userName,$pass,$db);
 
-                $sql = "SELECT * FROM AIRPORT;";
-                $result = mysqli_query($con, $sql);
+                $sql = "SELECT * FROM FLIGHT;";
+                $aiport_sql = "SELECT * FROM AIRPORT;";
+                $result = mysqli_query($con, $aiport_sql);
                 $resultCheck = mysqli_num_rows($result);
+
+
+                $airport_list = mysqli_fetch_assoc($result);
+                mysqli_free_result($result);
+
+                $result = mysqli_query($con, $ql);
+
                 if ($resultCheck > 0)
                 {
+                    
                     while($row = mysqli_fetch_assoc($result)){
-                        echo "<option>" . $row["CITY"] . "</option>";
+                        echo "<option>" . "From: " . $airport_list['1'] . " Leaving at: " . $row["DEPARTURE_TIME"] . " going to " . $airport_list['1'] . "Boarding: " . $row["BOARDING_TIME"] . "</option>";
+
                     }
                 }
                 $con->close();
