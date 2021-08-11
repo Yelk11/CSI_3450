6c76cfc3281b7e4e08dd67b8a948e7544835fe66
Yelk11 <neighbour.matthew@gmail.com>
2021-08-11 07:21:23 -0400

    update

--- Flights.php
+++ Flights.php
@@ -2,14 +2,11 @@
 <html>
 
 <body>
-    <!-- <form action="view flight_info.php" method="post">
+    <form action="view flight_info.php" method="post">
         <select>
             
-        </select>
 
-        <input type="submit">
-    </form> -->
-    <?php
+ <?php
 
         include "password.php";
         ini_set('display_errors', 1);
@@ -22,28 +19,29 @@
         $source_city = $_POST["source"];
 
         $sql = "SELECT * FROM AIRPORT WHERE CITY = \"$source_city\"";
-        
+         
 
         $result = mysqli_query($con, $sql);
         $resultCheck = mysqli_num_rows($result);
 
         if ($resultCheck > 0)
         {
-            $row = mysqli_fetch_assoc($result));
+            $row = mysqli_fetch_assoc($result) || trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error($con), E_USER_ERROR);
             $source_airport_id = $row["AIRPORT_ID"];
 
             mysqli_free_result($result);
 
-            //$flight_sql = "SELECT CITY FROM AIRPORT WHERE AIRPORT_ID = (SELECT DESTINATION_AIRPORT FROM FLIGHTS WHERE SOURCE_AIRPORT = $source_airport_id)";
-            $flight_sql = "SELECT DESTINATION_AIRPORT FROM FLIGHTS WHERE SOURCE_AIRPORT = 1";
+            $flight_sql = "SELECT CITY FROM AIRPORT WHERE AIRPORT_ID = (SELECT DESTINATION_AIRPORT FROM FLIGHT WHERE SOURCE_AIRPORT = $source_airport_id)";
+            
+            
             
             $result = mysqli_query($con, $sql);
             $resultCheck = mysqli_num_rows($result);
             
             if ($resultCheck > 0)
             {
-                while($row = mysqli_fetch_assoc($result))){
-                    echo "<option>" . $row["DESTINATION_AIRPORT"] . "</option>";
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<option>" . $row["CITY"] . "</option>";
                 }
             }
             else{
@@ -55,6 +53,10 @@
             
         }
     ?>
+            </select>
+
+<input type="submit">
+</form>
 </body>
 
 </html>
