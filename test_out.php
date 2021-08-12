0c4aca5520bd1664710b50b94f060aa958a8fd73
Yelk11 <neighbour.matthew@gmail.com>
2021-08-11 20:58:29 -0400

    Update test_out.php

--- test_out.php
+++ test_out.php
@@ -1,7 +1,21 @@
 
 
 
-<?php print_r($_POST); ?>
+<table>
+<?php 
 
 
+    foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
 
+
+?>
+</table>
